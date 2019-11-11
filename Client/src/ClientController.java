8b2594744f47565adb4fd1f0b663e6fc30b2e2d0
evanhenderson <ehenderson@zagmail.gonzaga.edu>
2019-11-10 21:55:28 -0800

    some socket setup init

--- Client/src/ClientController.java
+++ Client/src/ClientController.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientController {
 
     protected ClientModel model;
@@ -12,5 +18,33 @@ public class ClientController {
 class Main {
     public static void main(String[] args) {
         new ClientController(new ClientModel());
+        String ipAddress = view.serverIP;
+        int portNumber = 8080;
+        try{
+            Socket clientSocket = new Socket(ipAddress, portNumber);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            while (true){
+                // read from stdIn and send whatever the user types to the server
+                if (stdIn.ready()) { // has data to  read (kb.hasNextLine())
+                    String userInput = stdIn.readLine();
+                    out.println(userInput);
+                }
+                if (in.ready()){
+                    // the server has sent something to us (client)
+                    String serverResponse = in.readLine();
+                    System.out.println("Server sent: " + serverResponse);
+                    if (serverResponse.equals("closing")){
+                        System.out.println("Terminating program... ");
+                        break;
+                    }
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        }
     }
 }
--- Client/src/ClientModel.java
+++ Client/src/ClientModel.java
@@ -1,2 +1,11 @@
 public class ClientModel {
+    public void sendData(){
+
+    }
+    public void receiveData(){
+
+    }
+    public void download(){
+
+    }
 }
