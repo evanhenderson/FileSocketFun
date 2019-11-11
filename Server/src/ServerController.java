0e5936633d87332f29eff3222aeb3057263bc802
evanhenderson <2018hendersone@mililanihs.k12.hi.us>
2019-11-10 20:29:20 -0800

    basic server structure

---
+++ Server/src/ServerController.java
@@ -0,0 +1,11 @@
+public class ServerController {
+    ServerModel model;
+    ServerController(ServerModel model){
+        this.model = model;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Server/src/ServerModel.java
@@ -0,0 +1,28 @@
+public class ServerModel {
+    String[] imageList;
+    String[][] userList;
+    public void sendData(){
+
+    }
+    public void receiveData(){
+
+    }
+    public boolean authenticate(String username, String password){
+
+    }
+    public void storeImage(){
+
+    }
+    public void retrieve(){
+
+    }
+    public String[] getImageList(){
+        return imageList;
+    }
+    public String[][] getUserList() {
+        return userList;
+    }
+    public void setImageList(){
+
+    }
+}
