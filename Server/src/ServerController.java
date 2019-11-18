1cd8063eade0364e86b77743ae2ad12e443351b0
evanhenderson <ehenderson@zagmail.gonzaga.edu>
2019-11-18 12:59:08 -0800

    socket progress 1

--- Server/src/ServerController.java
+++ Server/src/ServerController.java
@@ -2,26 +2,51 @@ import java.net.*;
 import java.io.*;
 public class ServerController {
     ServerModel model;
-    ServerSocket serverSocket = new ServerSocket(8080);
-    Socket clientSocket = serverSocket.accept();
+    try{
+        ServerSocket serverSocket = new ServerSocket(8080);
+    }catch(IOException exception){
+        exception.printStackTrace();
+    }
+    try{
+        Socket clientSocket = serverSocket.accept();
+    }catch(IOException exception){
+        exception.printStackTrace();
+    }
     ServerController(ServerModel model){
         this.model = model;
 
     }
     public void start(){
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String inputLine;
-        while ((inputLine = in.readLine()) != null){
-            model.receiveData(inputLine, in, out);
+        try {
+            OutputStream out = new FileOutputStream("C:\\Users\\evanh\\OneDrive\\" +
+                    "Documents\\GitHub\\FileSocketFun\\" +
+                    "Server\\files\\file.jpg");
+        }catch(IOException exception){
+            exception.printStackTrace();
         }
+        try {
+            InputStream in = clientSocket.getInputStream();
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
+        int count;
+        model.receiveData(in, out);
         in.close();
         out.close();
         clientSocket.close();
     }
-    public static void main(String[] args) {
-       ServerController controller =  new ServerController(new ServerModel);
-       controller.start();
+    public static void main(String[] args) throws IOException {
+       new Thread(){
+           public void run(){
+               try{
+                   ServerController controller =  new ServerController(new ServerModel);
+                   controller.start();
+               }catch(IOException e){
+                   e.printStackTrace();
+               }
+           }
+       }.start();
+
 
     }
 }
--- Server/src/ServerModel.java
+++ Server/src/ServerModel.java
@@ -1,13 +1,24 @@
+import java.nio.*
 public class ServerModel {
     String[] imageList;
     String[][] userList;
     public void sendData(String Msg, BufferedReader in, PrintWriter out){
         out.println(Msg);
     }
-    public void receiveData(String clientMsg, BufferedReader in, PrintWriter out){
-        if (clientMsg == "stop"){
-            String Msg = "goodbye";
-            sendData(Msg, in, out);
+    public void receiveData(InputStream in, FileOutputStream out){
+        byte[] bytes = new byte[16*1024];
+        int count;
+        for(int i = 0; i < 4; i++){
+            in.read(bytes);
+            bytes[] code = new byte[4];
+            code[i] = bytes[i];
+        }
+        ByteBuffer wrapped = ByteBuffer.wrap(code);
+        int num = wrapped.getInt();
+        if(num == 2) {
+            while (count = in.read(bytes) != -1) {
+                out.write(bytes, 0, count);
+            }
         }
     }
     public boolean authenticate(String username, String password){
